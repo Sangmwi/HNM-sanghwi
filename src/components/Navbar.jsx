2247b712fc87bc424300166100e3e7a30a7f2f27
Sangmwi <gkstkdgnl12@naver.com>
2025-04-10 14:17:28 +0900

    2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faTimes, faBars } from "@fortawesome/free-solid-svg-icons";
 import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 
-const Navbar = ({ isLoggedIn, setIsLoggedIn, onSearch, initialSearchQuery }) => {
+const Navbar = ({
+  isLoggedIn,
+  setIsLoggedIn,
+  onSearch,
+  initialSearchQuery,
+}) => {
   const [isSearchVisible, setIsSearchVisible] = useState(false);
-  const [searchQuery, setSearchQuery] = useState(initialSearchQuery || '');
+  const [searchQuery, setSearchQuery] = useState(initialSearchQuery || "");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
-    setSearchQuery(initialSearchQuery || '');
+    setSearchQuery(initialSearchQuery || "");
   }, [initialSearchQuery]);
 
-  const menuList = [
-    "Women",
-    "Men",
-    "Baby",
-    "Kids",
-    "Home",
-  ];
+  const menuList = ["Women", "Men", "Baby", "Kids", "Home"];
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -36,51 +36,66 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn, onSearch, initialSearchQuery }) => 
     setIsSearchVisible(!isSearchVisible);
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   const handleLogout = () => {
     setIsLoggedIn(false);
-    navigate('/');
+    navigate("/");
   };
 
   return (
     <nav className="navbar">
-      <div className="login-button-div">
-        {isLoggedIn ? (
-          <button onClick={handleLogout}>
-            <FontAwesomeIcon icon={faUser} />
-            <span>로그아웃</span>
-          </button>
-        ) : (
-          <Link to="/login">
-            <button>
-              <FontAwesomeIcon icon={faUser} />
-              <span>로그인</span>
-            </button>
-          </Link>
-        )}
-        <div className="search-div-media">
-          <div className="search-icon" onClick={toggleSearch}>
-            <FontAwesomeIcon icon={faSearch} />
+      <div className="top-bar">
+        <div className="left-side">
+          <div className={`slide-menu ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+            <ul>
+              {menuList.map((menu, index) => (
+                <li key={index}>{menu}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="hamburger-menu" onClick={toggleMenu}>
+            <FontAwesomeIcon icon={faBars} />
+          </div>
+        </div>
+        <div className="right-side">
+          <div className="login-button-div">
+            {isLoggedIn ? (
+              <button onClick={handleLogout}>
+                <FontAwesomeIcon icon={faUser} />
+                <span>로그아웃</span>
+              </button>
+            ) : (
+              <Link to="/login">
+                <button>
+                  <FontAwesomeIcon icon={faUser} />
+                  <span>로그인</span>
+                </button>
+              </Link>
+            )}
+            <div className="search-div-media">
+              <div className="search-icon" onClick={toggleSearch}>
+                <FontAwesomeIcon icon={faSearch} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
       <div className="logo">
         <Link to="/">
-          <img 
+          <img
             src="https://images.seeklogo.com/logo-png/6/2/hm-logo-png_seeklogo-64496.png"
             alt="logo"
           />
         </Link>
       </div>
       <div className="menu-and-search">
-        <ul className="menu-list">
-          {menuList.map((menu, index) => (
-            <li key={index}>{menu}</li>
-          ))}
-        </ul>
         <form className="search-form" onSubmit={handleSearch}>
-          <input 
-            type="text" 
-            placeholder="검색" 
+          <input
+            type="text"
+            placeholder="검색"
             value={searchQuery}
             onChange={handleSearchChange}
           />
@@ -89,7 +104,10 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn, onSearch, initialSearchQuery }) => 
           </button>
         </form>
       </div>
-      <form className={`search-form-mobile ${isSearchVisible ? 'active' : ''}`} onSubmit={handleSearch}>
+      <form
+        className={`search-form-mobile ${isSearchVisible ? "active" : ""}`}
+        onSubmit={handleSearch}
+      >
         <input
           type="text"
           placeholder="검색"
